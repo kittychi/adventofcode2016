4399b7ed37115fd696787bb316e1a5994b8d23b9
kittychi <kitty.sham@gmail.com>
2016-12-03 21:00:39 -0800

    day 3 part 2

--- day3.py
+++ day3.py
@@ -3,11 +3,25 @@ import inputs
 def day3(triangles):
 	num_triangles = 0
 	for triangle in triangles.split('\n'):
-		# print(triangle)
 		sides = [int(side) for side in triangle.split()]
 		perimeter = sum(sides)
 		largest_side = max(sides)
 		if (perimeter - largest_side > largest_side): 
+			num_triangles = num_triangles + 1
+	print(num_triangles)
+
+def day3b(triangles_list):
+	num_triangles = 0
+	triangles = triangles_list.split('\n')
+	
+	triangles = [[row.split()[i] for row in triangles] for i in range(3)]
+	print(triangles)
+	triangles = [one[x:x+3] for x in range(0, len(triangles[1]), 3) for one in triangles]
+	print(triangles)
+	for triangle in triangles: 
+		print(triangle)
+		sides = [int(side) for side in triangle]
+		if (sum(sides) - max(sides) > max(sides)):
 			num_triangles = num_triangles + 1
 	print(num_triangles)
 
@@ -21,6 +35,8 @@ test2 = '''  330  143  338
    15  881  210
   662   15   70
   273  277  707
-   50  592  770'''
+   50  592  770
+  280  313  407'''
 day3(inputs.day3input)
+day3b(inputs.day3input)
 
