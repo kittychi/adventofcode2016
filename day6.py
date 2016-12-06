1d64ff93a5dcc2239ac476e8a868dd6aa1eb6f76
kittychi <kitty.sham@gmail.com>
2016-12-05 21:15:03 -0800

    day 6 part 2

--- day6.py
+++ day6.py
@@ -4,9 +4,14 @@ def day6(input_lines):
 	lines = input_lines.split()
 	chars = [''.join([a[i] for a in lines]) for i in range(len(lines[0]))]
 
+	part1 = ''
+	part2 = ''
 	for char in chars:
-		freq_ = collections.Counter(char).most_common(1)
-		print(freq_)
+		freq_ = collections.Counter(char).most_common()
+		part1 += freq_[0][0]
+		part2 += freq_[-1][0]
+	
+	print(part1, part2)
 
 
 test = ''' eedadn
@@ -25,4 +30,6 @@ test = ''' eedadn
 	vrdear
 	dvrsen
 	enarar '''
+
+day6(test)
 day6(inputs.day6input)
