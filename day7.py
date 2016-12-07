4231a86841926a56841b456c516e208b38c2a605
kittychi <kitty.sham@gmail.com>
2016-12-06 23:37:18 -0800

    day 7 part 2 - UGH

--- day7.py
+++ day7.py
@@ -3,24 +3,47 @@ import inputs, re
 def day7(address_list):
 	addresses = address_list.split()
 
-	count = 0
-	for address in addresses: 
+	tls_count = 0
+	for address in addresses:
 		invalid = re.search('\[[a-z]*(([a-z])([a-z])\\3\\2)[a-z]*\]', address)
 		if invalid: 
-			print(invalid.group(1), address)
 			if invalid.group(1)[0] != invalid.group(1)[1]:
 				continue
 		outsides = re.split('\\[[a-z]*\\]', address)
 		for outside in outsides: 
 			valid = re.search('(([a-z])([a-z])\\3\\2)', outside)
 			if valid and valid.group(1)[0] != valid.group(1)[1]: 
-				count += 1
-				print(count, valid.group(1), address)
+				tls_count += 1
 				break
+	print(tls_count)
+
+def day7b(address_list):
+	addresses = address_list.split()
+
+	ssl_count = 0
+	for address in addresses: 
+		braces = re.finditer(r'\[([a-z]*)\]', address)
+		matched = []
+		for brace in braces:
+			matched.extend(re.findall(r'(([a-z])(?=([a-z])\2))', brace.group(1)))
+		for match, a, b in matched: 
+			if a == b:
+				continue
+			outsides = re.split('\\[[a-z]*\\]', address)
+			bab = b+a+b
+			if any(bab in outside for outside in outsides): 
+				ssl_count+=1
+				break
+	print(ssl_count)
+				
+
 
 test= ''' abba[mnop]qrst
 	abcd[bddb]xyyx
 	aaaa[qwer]tyui
 	ioxxoj[asdfgh]zxcvbn '''
 
+test2 = ''' nesmourutitzqtolwd[rurfefjvljejcufm]jagkqdwpkefkjdz[cctohikipqxyxbdjxsg]badmffkslhmgsxqscf '''
+
 day7(inputs.day7input)
+day7b(inputs.day7input)
