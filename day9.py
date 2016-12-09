683cef28aeeb9b49008f39a8d2a86eef80949614
kittychi <kitty.sham@gmail.com>
2016-12-08 22:35:15 -0800

    day 9 part 2

--- day9.py
+++ day9.py
@@ -1,4 +1,4 @@
-import inputs
+import inputs, re
 
 def day9(compressed):
 	decompressed = ''
@@ -21,6 +21,37 @@ def day9(compressed):
 
 	print(len(decompressed), decompressed)
 	
-test= 'X(8x2)(3x3)ABCY'
+def day9b(compressed):
+	markers = re.findall(r'\((\dx\d)\)', compressed)
+	decompressed_count = 0
+	cur_index = 0
+	while len(markers) > 0: 
+		marker = markers.popleft()
+		open_ = compressed[cur_index:].find(marker)
 
-day9(inputs.day9input)
+
+def get_decompress_count(compressed, multiplier):
+	markers = re.findall(r'\((\d*x\d*)\)', compressed)
+	# print(markers, compressed)
+	if len(markers) == 0:
+		# print('-', compressed, multiplier, 'returning', len(compressed) * multiplier)
+		return len(compressed) * multiplier
+	markers.reverse()
+	marker = markers.pop()
+	count, mul = list(map(lambda x: int(x),marker.split('x')))
+	# print('', mul, multiplier)
+	start_bracket = compressed.find(marker) - 1
+	end_bracket = start_bracket + len(marker) + 1
+	left = get_decompress_count(compressed[end_bracket+1:end_bracket+count+1], mul)
+	right = get_decompress_count(compressed[end_bracket+count+1:], multiplier)
+	# print(compressed, 'left', left, 'right', right)
+	return start_bracket * multiplier + right + (left*multiplier) 
+
+test= 'X(8x2)(3x3)ABCY'
+test2 ='(27x12)(20x12)(13x14)(7x10)(1x12)A'
+test3 = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
+# # day9b(test)
+# day9b(inputs.day9input)
+
+print(get_decompress_count(inputs.day9input, 1))
+
