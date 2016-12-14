96166519210e7cf9c4f1e0cbf796ab3e24f46b41
kittychi <kitty.sham@gmail.com>
2016-12-13 23:04:59 -0800

    day 14 part 2

--- day14.py
+++ day14.py
@@ -1,21 +1,20 @@
 from collections import defaultdict 
 import inputs, hashlib, re
 
-def day14(salt): 
+def day14(salt,times): 
 	cur_index = 0
 	keys = []
 	pos_keys = defaultdict(list)
 	while len(keys) < 65:
 		to_hash = salt+str(cur_index)
-		m = hashlib.md5()
-		m.update(to_hash.encode('utf-8'))
-		hashed = m.hexdigest()
+		hashed = hash_plz(to_hash, times)
 		three = re.findall(r'((\w)\2{2})', hashed)
 		five = re.findall(r'((\w)\2{4})', hashed)
 		for _, letter in five:
 			key = sorted([pkey for pkey in pos_keys[letter] if pkey+1000 > cur_index])
-			# print(letter, cur_index, pos_keys[letter])
+			print(letter, cur_index, key, pos_keys[letter])
 			keys.extend(key)
+			pos_keys[letter] = []
 
 		if three:
 			pos_keys[three[0][1]].append(cur_index)
@@ -23,7 +22,16 @@ def day14(salt):
 
 	print(len(keys), sorted(keys)[63], sorted(keys))
 
-day14('abc')
+def hash_plz(to_hash, times):
+	for i in range(times):
+		m = hashlib.md5()
+		m.update(to_hash.encode('utf-8'))
+		to_hash = m.hexdigest()
+	return to_hash
 
-day14(inputs.day14)
+# day14('abc', 2017)
+
+day14(inputs.day14, 1)
+
+day14(inputs.day14, 2017)
 
