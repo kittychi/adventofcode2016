bad940108ddb4127d56efb0104ba965b19454681
kittychi <kitty.sham@gmail.com>
2016-12-19 14:42:49 +0800

    day 19 part 2 no leaderboard :(

--- day19.py
+++ day19.py
@@ -6,4 +6,18 @@ def day19(number):
 	pos = int(bin, 2)
 	print(pos)
 
-day19(3014603)
+def day19b(number):
+	elves = [1 + i for i in range(number)]
+	while len(elves) > 4:
+		half = len(elves) // 2
+		if len(elves) % 2 == 0: 
+			halved = elves[half-1::3]
+			num_removed = len(elves[half-1:]) - len(halved)
+			elves = elves[num_removed:half-1] + halved + elves[:num_removed]
+		else:
+			halved = elves[half+1::3]
+			num_removed = len(elves[half:]) - len(halved)
+			elves = elves[num_removed:half] + halved + elves[:num_removed]
+	print(elves[0])
+
+day19b(inputs.day19)
